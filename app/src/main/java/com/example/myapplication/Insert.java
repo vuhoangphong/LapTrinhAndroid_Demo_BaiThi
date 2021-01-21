47901588ed5b30810cc165a419d7335a6249470a
vhp159357 <52731971+vhp159357@users.noreply.github.com>
2021-01-21 11:09:03 +0700

    Update Button

--- app/src/main/java/com/example/myapplication/Insert.java
+++ app/src/main/java/com/example/myapplication/Insert.java
@@ -4,6 +4,8 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Insert extends AppCompatActivity {
 
@@ -11,5 +13,17 @@ public class Insert extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert);
+
+
     }
+    public void HideButton(View view){
+        Button btnEdit = (Button)findViewById(R.id.edit);
+        Button btnDelete = (Button)findViewById(R.id.xoa);
+        Button btnSave = (Button)findViewById(R.id.save);
+
+        btnEdit.setVisibility(View.VISIBLE);
+        btnDelete.setVisibility(View.VISIBLE);
+        btnSave.setVisibility(View.INVISIBLE);
+    }
+
 }
