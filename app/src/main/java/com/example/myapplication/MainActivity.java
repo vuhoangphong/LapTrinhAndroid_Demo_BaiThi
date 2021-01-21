029dcdd0e40e2df054fe25675a977fe4ca3285f2
locdaica2345 <73575518+locdaica2345@users.noreply.github.com>
2021-01-21 10:55:20 +0700

    comit
    asdas

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    //khởi tạo menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+
+        return true;
+    }
+
+    //chức năng item trong menu
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        if (item.getItemId() == R.id.Insert) {
+            //sử dụng startActivityForResult để lấy kết quả từ Insert trả về, requestCode từ Main -> Insert là 1
+            final int result = 1;
+            startActivityForResult(new Intent(this, Insert.class), result);
+
+            /*
+             * //hoặc viết theo cách sau:
+             *  Intent intent = new Intent(this,InsertActivity.class);
+             * startActivityForResult(intent, result);
+             *
+             * */
+        }
+        return true;
+    }
+
 }
